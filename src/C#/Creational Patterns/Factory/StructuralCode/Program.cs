ed266eb680084c453c464e000edabc5f64ed971d
plamenna-petrova <plamennavp@abv.bg>
2024-02-19 12:13:20 +0200

    Added forgotten Factory pattern StructureCode

--- src/C#/Creational Patterns/Factory/StructuralCode/Program.cs
+++ src/C#/Creational Patterns/Factory/StructuralCode/Program.cs
@@ -2,11 +2,56 @@
 
 namespace StructuralCode
 {
-    internal class Program
+    public abstract class Product
+    {
+
+    }
+
+    public class ConcreteProductA : Product
+    {
+
+    }
+
+    public class ConcreteProductB : Product
+    {
+
+    }
+
+    public abstract class Creator
+    {
+        public abstract Product CreateProduct();
+    }
+
+    public class ConcreteCreatorA : Creator
+    {
+        public override Product CreateProduct()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    public class ConcreteCreatorB : Creator
+    {
+        public override Product CreateProduct()
+        {
+            return new ConcreteProductB();
+        }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            foreach (Creator creator in creators)
+            {
+                Product createdProduct = creator.CreateProduct();
+                Console.WriteLine($"Created: {createdProduct.GetType().Name}");
+            }
         }
     }
 }
