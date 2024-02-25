f17ae4a33612ca7e209336112c728d3669de9dcc
plamenna.petrova <plamennavp@abv.bg>
2024-02-25 19:44:10 +0200

    Solution fix

--- src/C#/CreationalPatterns/Builder/StructuralCode/Program.cs
+++ src/C#/CreationalPatterns/Builder/StructuralCode/Program.cs
@@ -6,7 +6,9 @@ namespace StructuralCode
     public abstract class Builder
     {
         public abstract void BuildPartA();
+
         public abstract void BuildPartB();
+
         public abstract Product GetResult();
     }
 
