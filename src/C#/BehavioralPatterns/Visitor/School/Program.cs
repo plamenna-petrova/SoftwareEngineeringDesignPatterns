fbb53d5bc2c8f7e2ca1b59188f9bf1ed16b69ccb
plamenna.petrova <plamennavp@abv.bg>
2024-09-13 22:15:20 +0300

    Added one more example for the Visitor pattern

---
+++ src/C#/BehavioralPatterns/Visitor/School/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School
+{
+    public interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class Kid : IElement
+    {
+        public Kid(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public interface IVisitor
+    {
+        void Visit(IElement element);
+    }
+
+    public class Doctor : IVisitor
+    {
+        public Doctor(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public void Visit(IElement element)
+        {
+            Kid kid = (Kid)element;
+            Console.WriteLine($"Doctor: {Name} did the health checkup of the child: {kid.Name}");
+        }
+    }
+
+    public class Teacher : IVisitor
+    {
+        public Teacher(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public void Visit(IElement element)
+        {
+            Kid kid = (Kid)element;
+            Console.WriteLine($"Teacher: {Name} assessed the daily work of the child: {kid.Name}");
+        }
+    }
+
+    public class School
+    {
+        private static readonly List<IElement> elements = new List<IElement>();
+
+        static School()
+        {
+            elements = new List<IElement>
+            {
+                new Kid("John"),
+                new Kid("Sarah"),
+                new Kid("Kim")
+            };
+        }
+
+        public void PerformOperation(IVisitor visitor)
+        {
+            foreach (var kid in elements)
+            {
+                kid.Accept(visitor);
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            School school = new School();
+
+            var doctor = new Doctor("James");
+            school.PerformOperation(doctor);
+            Console.WriteLine();
+
+            var teacher = new Teacher("Mr. Smith");
+            school.PerformOperation(teacher);
+        }
+    }
+}
--- src/PHP/BehavioralPatterns/Visitor/School/main.php
+++ src/PHP/BehavioralPatterns/Visitor/School/main.php
@@ -1,1 +1,111 @@
 <?php
+
+interface IElement {
+    public function accept(IVisitor $visitor);
+}
+
+class Kid implements IElement {
+    private string $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function accept(IVisitor $visitor): void
+    {
+        $visitor->visit($this);
+    }
+}
+
+interface IVisitor {
+    public function visit(IElement $element);
+}
+
+class Doctor implements IVisitor {
+    private string $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function visit(IElement $element): void
+    {
+        if ($element instanceof Kid) {
+            $kid = $element;
+            echo "Doctor: " . $this->name . " did the health checkup of the child: " . $kid->getName() . PHP_EOL;
+        }
+    }
+}
+
+class Teacher implements IVisitor {
+    private string $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function visit(IElement $element): void
+    {
+        if ($element instanceof Kid) {
+            $kid = $element;
+            echo "Teacher: " . $this->name . " assesses the daily work of the child: " . $kid->getName() . PHP_EOL;
+        }
+    }
+}
+
+class School {
+    private array $elements;
+
+    public function __construct() {
+        $this->elements = [
+            new Kid("John"),
+            new Kid("Sarah"),
+            new Kid("Kim")
+        ];
+    }
+
+    public function performOperation(IVisitor $visitor): void
+    {
+        foreach ($this->elements as $element) {
+            $element->accept($visitor);
+        }
+    }
+}
+
+$school = new School();
+
+$doctor = new Doctor("James");
+$school->performOperation($doctor);
+echo PHP_EOL;
+
+$teacher = new Teacher("Mr. Smith");
+$school->performOperation($teacher);
